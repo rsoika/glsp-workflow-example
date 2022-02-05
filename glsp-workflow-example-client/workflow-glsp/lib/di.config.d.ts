1a5438bc02cac2f3554e772bc2b76bcff22d4300
Ralph Soika <ralph.soika@imixs.com>
2022-02-05 23:12:17 +0100

    update

--- glsp-workflow-example-client/workflow-glsp/lib/di.config.d.ts
+++
@@ -1,6 +0,0 @@
-import 'balloon-css/balloon.min.css';
-import { Container } from 'inversify';
-import 'sprotty/css/edit-label.css';
-import '../css/diagram.css';
-export default function createContainer(widgetId: string): Container;
-//# sourceMappingURL=di.config.d.ts.map
